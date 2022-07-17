f0672c18ba1bdd41a6cc2ee2fcb63a2b81e70252
João Manaia <joaopfmanaia@gmail.com>
2022-07-17 18:56:11 +0100

    added mui and tailwidcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { ThemeProvider } from "@mui/material/styles"
+import { CssBaseline } from "@mui/material"
+import Head from "next/head"
+import theme from "../app/theme/theme"
+import { CacheProvider, EmotionCache } from "@emotion/react"
+import createEmotionCache from "../app/theme/createEmotionCache"
+import React from "react"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+function MyApp({ Component, pageProps, emotionCache }: MyAppProps) {
+  return (
+    <CacheProvider value={emotionCache || clientSideEmotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
